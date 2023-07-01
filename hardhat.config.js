0d6b4e7878941763197e8701100611f74b473bbf
arjunadhikary <reasonworld12345@gmail.com>
2023-07-01 18:59:54 +0545

    deploy script added

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     sepolia: {
       url: "https://eth-sepolia.g.alchemy.com/v2/q6rKG-ilP2jaVLhY0ShuPp_oDNw7S8xW",
-      accounts: ["59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d"]
+      accounts: ["59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d", "59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d", "59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d"]
     },
   },
   paths: {
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,9 +1,25 @@
-const main = () => {
-    
+const { ethers } = require("hardhat");
+
+const main = async () => {
+
+    const [deployer, beneficiary, arbiter] = await ethers.getSigners();
+    console.log(
+        "Deploying contracts with the account:",
+        deployer.address
+    );
+
+    console.log("Account balance:", (await deployer.getBalance()).toString());
+
+    const Escrow = await ethers.getContractFactory("Escrow");
+    const escrow = await Escrow.deploy(arbiter.address, beneficiary.address, {
+        value: ethers.utils.parseEther('1')
+    });
+    console.log("Token address:", escrow.address);
+
 }
 
 
 main().then(dt => process.exit(0)).catch(err => {
-    console.log(error);
+    console.log(err);
     process.exit(1)
 })
